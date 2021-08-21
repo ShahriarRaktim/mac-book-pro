a5fdc3b0d2dbfafc97b5e941cf06349bc172844b
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-08-21 13:22:03 +0600

    completed

--- js/mac.js
+++ js/mac.js
@@ -1,7 +1,11 @@
-function costFixed(costId, cost) {
+//  function for fixing extra cost 
+
+function extraCostFixed(costId, cost) {
     const memory = document.getElementById(costId +'-cost');
     memory.innerText = cost;
 }
+
+//  function for fixing total price 
 
 function totalFixed() {
     const lastTotal = document.getElementById('last-total');
@@ -15,32 +19,47 @@ function totalFixed() {
     lastTotal.innerText = total;
 }
 
+//  Start Event Handler from here 
 
 document.getElementById('memory-8gb').addEventListener('click', function () {
-    costFixed('memory', 0);
+    extraCostFixed('memory', 0);
     totalFixed();
 })
 document.getElementById('memory-16gb').addEventListener('click', function () {
-    costFixed('memory', 180);
+    extraCostFixed('memory', 180);
     totalFixed();
 })
 document.getElementById('stroge-256gb').addEventListener('click', function () {
-    costFixed('stroge', 0);
+    extraCostFixed('stroge', 0);
     totalFixed();
 })
 document.getElementById('stroge-512gb').addEventListener('click', function () {
-    costFixed('stroge', 100);
+    extraCostFixed('stroge', 100);
     totalFixed()
 })
 document.getElementById('stroge-1tb').addEventListener('click', function () {
-    costFixed('stroge', 180);
+    extraCostFixed('stroge', 180);
     totalFixed()
 })
 document.getElementById('late-delivary').addEventListener('click', function () {
-    costFixed('delivary', 0);
+    extraCostFixed('delivary', 0);
     totalFixed()
 })
 document.getElementById('first-delivary').addEventListener('click', function () {
-    costFixed('delivary', 20);
+    extraCostFixed('delivary', 20);
     totalFixed()
+})
+
+//  Event handler for promo code 
+
+document.getElementById('button-addon2').addEventListener('click', function () {
+    const input = document.getElementById('input-promo');
+    const lastTotal = document.getElementById('last-total');
+    const lastTotalText = lastTotal.innerText;
+    const inputPromo = input.value;
+    if(inputPromo == 'stevekaku'){
+        const promoTotal = parseInt(lastTotalText)*0.8;
+        lastTotal.innerText = promoTotal;
+        input.value = '';
+    }
 })
